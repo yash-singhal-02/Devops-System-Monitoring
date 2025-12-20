0291e18803e5a76082aa37a5fc9a5e619d211d15
Yash <y.yashsinghal207@gmail.com>
2025-12-20 22:27:33 +0530

    Added network configuration details

--- system_report.sh
+++ system_report.sh
@@ -4,3 +4,7 @@
 
 echo "System Report" > system_report.txt
 date >> system_report.txt
+
+echo "" >> system_report.txt
+echo "Network Configuration:" >> system_report.txt
+ifconfig >> system_report.txt
